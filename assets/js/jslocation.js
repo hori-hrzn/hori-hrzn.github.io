ec4c5c275641cf58c7e1755d13a7bbe1b7d0d627
Hori <hori_hrzn@proton.me>
2022-07-09 11:53:31 +0200

    big commit, cleaning js files, updating language pages and estructure of the web

--- assets/js/jslocation.js
+++ assets/js/jslocation.js
@@ -1,11 +1,12 @@
+// This script is embedded in the main index.html file, and when it loads, it redirects the user to the correct language page
+
 let lang = navigator.language
-
-console.log(lang)
 // ca-valencia, ca, es, en...
-if (lang=="es"){
-    window.location.replace("./es/")
-}
-else if (lang=="en"){
-    window.location.replace("./en/")
 
+if (lang == "es") {
+    window.location.replace("./es/")
+} else if (lang == "ca" || lang == "ca-valencia") {
+    window.location.replace("./val/")
+} else {
+    window.location.replace("./en/")
 }
